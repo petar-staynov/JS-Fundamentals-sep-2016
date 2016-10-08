1a4f509dc619850cd9a21f4566d2409c7c29e99b
Petar Staynov <petar.staynov@gmail.com>
2016-10-09 00:08:22 +0300

    asd

---
+++ 09. Arrays and Matrices Exercises/07. Magic Matrices.js
@@ -0,0 +1,16 @@
+function main(matrixRows) {
+    let matrix = matrixRows.map(row => row.split(' ').map(Number));
+
+    
+
+    for (let row = 0; row < matrix.length; row++)
+    {
+        let cols = matrix[row];
+        for (let col = 0; col < cols.length; col++)
+        {
+
+        }
+    }
+    console.log(neighbours);
+
+}
